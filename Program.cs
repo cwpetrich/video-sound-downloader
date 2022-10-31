13a9df96182a5035960d18ac34110ec5652d17ea
cwpetrich <conradpetrich@gmail.com>
2022-10-31 16:39:16 -0600

    Updated script to use multiple URLs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ public class Program
     public static async Task Main(string[] args)
     {
         var youtubeClient = new YoutubeClient();
-        var argument = new Argument<string>("Url", "This is the url of a video found on youtube that you want to stream.");
+
+        var argument = new Argument<string[]>("Urls", "These are the urls of videos found on youtube that you want to download.");
         var audioOption = new Option<bool>("--audio", "Download only the audio of the given url.");
         var videoOption = new Option<bool>("--video", "Download only the video of the given url.");
         var outputOption = new Option<string?>(new string[]{"-o","--output"}, () => null, "The path to the directory where the output should be sent.");
@@ -63,36 +64,50 @@ public class Program
         await rootCommand.InvokeAsync(args);
     }
 
-    private static async Task GetVideoWithAudio(YoutubeClient youtubeClient, string url, string? name = null)
+    private static async Task GetVideoWithAudio(YoutubeClient youtubeClient, IEnumerable<string> urls, string? name = null)
     {
-        var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(url);
+        foreach (var url in urls)
+        {
+            var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(url);
 
-        // Get highest quality muxed stream
-        var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+            // Get highest quality muxed stream
+            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
 
-        await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, $"{name ?? "video"}.{streamInfo.Container}");
+            await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, $"{name ?? "video"}.{streamInfo.Container}");
+        }
     }
 
-    private static async Task GetVideo(YoutubeClient youtubeClient, string url, string? name = null)
+    private static async Task GetVideo(YoutubeClient youtubeClient, IEnumerable<string> urls, string? name = null)
     {
-        var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(url);
+        foreach (var url in urls)
+        {
+            var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(url);
 
-        // ...or highest quality MP4 video-only stream
-        var streamInfo = streamManifest
-            .GetVideoOnlyStreams()
-            .Where(s => s.Container == Container.Mp4)
-            .GetWithHighestVideoQuality();
+            // ...or highest quality MP4 video-only stream
+            var streamInfo = streamManifest
+                .GetVideoOnlyStreams()
+                .Where(s => s.Container == Container.Mp4)
+                .GetWithHighestVideoQuality();
 
-        await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, $"{name ?? "video"}.{streamInfo.Container}");
+            if (streamInfo == null)
+            {
+                throw new Exception("There weren't any Mp4 containers.");
+            }
+
+            await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, $"{name ?? "video"}.{streamInfo.Container}");
+        }
     }
 
-    private static async Task GetAudio(YoutubeClient youtubeClient, string url, string? name = null)
+    private static async Task GetAudio(YoutubeClient youtubeClient, IEnumerable<string> urls, string? name = null)
     {
-        var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(url);
+        foreach (var url in urls)
+        {
+            var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(url);
 
-        // ...or highest bitrate audio-only stream
-        var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+            // ...or highest bitrate audio-only stream
+            var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
 
-        await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, $"{name ?? "audio"}.{streamInfo.Container}");
+            await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, $"{name ?? "audio"}.{streamInfo.Container}");
+        }
     }
 }
